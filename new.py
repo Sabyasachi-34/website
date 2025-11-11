c309f60d64434fef1d27cf9a7ed6278356fdcab9
Sabyasachi Das <dsabyasachi34@gmail.com>
2025-11-11 23:02:11 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.title ('welcome to my world')
+st.write('Myself Sabya sachi Das .i am from Odisha')
+st.write('Thank you for opening this link')
+if st.button ('your Gift'):
+   st.write('(っ◔◡◔)っ  you are amazing ✌')
+st.markdown('------')
+st.write('Devloped by[sabyasachi]')
+st.write('Data scientist')
