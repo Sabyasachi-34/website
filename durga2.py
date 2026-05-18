54023b156673020f6fddb2e0206465508116cdaf
Sabyasachi Das <dsabyasachi34@gmail.com>
2026-05-18 15:18:44 +0530

    Update durga2.py

--- durga2.py
+++ durga2.py
@@ -438,7 +438,7 @@ current_year = datetime.now().year
 st.markdown(
     f"""
     <div class='footer'>
-    © {current_year} DURGA AUTOMOBILE | Designed by Sabyasachi 🚜
+    © {current_year} DURGA AUTOMOBILE | Designed by deVSabya 🚜
     </div>
     """,
     unsafe_allow_html=True
